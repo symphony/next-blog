ac129d5b78ac1a8febe7afe07c538276d6100bf8
symphony <jciurc@gmail.com>
2022-09-05 11:56:24 -0700

    set up hydration in app.ts

--- pages/_app.ts
+++ pages/_app.ts
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import { useState } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { config } from 'lib/react-query-config';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+    // This ensures that data is not shared
+    // between different users and requests
+    const [queryClient] = useState(() => new QueryClient(config))
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            // Hydrate query cache
+            <Hydrate state={pageProps.dehydratedState}>
+                <Component  {...pageProps} />
+            </Hydrate>
+        </QueryClientProvider>
+    )
+
 }
 
-export default MyApp
+export default MyApp;
